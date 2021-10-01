2feeef34034ae7dc9e5756d0a9ee5b0eee66fea4
soya-s2 <soya.s2.dev@gmail.com>
2021-10-01 23:56:16 +0900

    Update 2021-09-30

---
+++ practice-file/2021-09-30-순열-1.js
@@ -0,0 +1,20 @@
+let input = ["a", "b", "c"];
+let count = 0;
+
+function permutation(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i == j) continue;
+      for (let k = 0; k < arr.length; k++) {
+        if (i == k) continue;
+        if (j == k) continue;
+
+        console.log(arr[i], arr[j], arr[k]);
+        count++;
+      }
+    }
+  }
+}
+
+permutation(input);
+console.log(count);
